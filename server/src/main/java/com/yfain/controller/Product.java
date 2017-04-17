e68a84d63fcdafd7eebbefd7bf75cd48380ce0b4
Yakov Fain <yakovfain@gmail.com>
2017-04-17 03:41:06 -0400

    initial commit

---
+++ server/src/main/java/com/yfain/controller/Product.java
@@ -0,0 +1,16 @@
+package com.yfain.controller;
+
+/**
+ * Created by yfain11 on 3/15/17.
+ */
+public class Product {
+    public int id;
+    public String title;
+    public double price;
+
+    public Product(int id, String title, double price){
+        this.id=id;
+        this.title=title;
+        this.price=price;
+    }
+}
---
+++ server/src/main/java/com/yfain/controller/ProductController.java
@@ -0,0 +1,57 @@
+package com.yfain.controller;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class ProductController {
+
+    Product products[] = new Product[3];
+
+    ProductController(){
+        products[0] = new Product(0,"First product", 59.99);
+        products[1] = new Product(1,"Second product", 12.50);
+        products[2] = new Product(2,"Third product", 14.40);
+    }
+
+    @RequestMapping(value="products",
+                    method = RequestMethod.GET,
+                    produces= MediaType.APPLICATION_JSON_VALUE)
+    public Product[] getProducts(){
+        return products;
+    }
+}
+
+
+
+
+
+/*
+package com.yfain.controller;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+
+    Product products[] = new Product[3];
+
+    ProductController(){
+        products[0] = new Product(0,"First product", 59.99);
+        products[1] = new Product(1,"Second product", 12.50);
+        products[2] = new Product(2,"Third product", 14.40);
+    }
+
+    @RequestMapping(method = RequestMethod.GET, produces= MediaType.APPLICATION_JSON_VALUE)
+    public Product[] getProducts(){
+         return products;
+    }
+}
+*/
